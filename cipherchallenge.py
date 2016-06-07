23aead125434eb83a436e0f2783de9b3adf343a7
Damien Gouriet <damien.gouriet@gmail.com>
2016-06-07 20:45:43 +0100

    Added ceasar and vigenere
    Cipher Challenge Stage 1, 2 and 4

---
+++ cipherchallenge.py
@@ -0,0 +1,332 @@
+# -*- coding: utf-8 -*-
+"""
+The Cipher challenge
+http://simonsingh.net/cryptography/cipher-challenge/the-ciphertexts/
+"""
+
+#Stage 1:
+
+import nltk
+import string
+import matplotlib.pyplot as plt
+import pylab as pl
+import numpy as np
+
+freqEnglish = {}    
+freqEnglish['a']=8.167	 
+freqEnglish['b']=1.492	 
+freqEnglish['c']=2.782	 
+freqEnglish['d']=4.253	 
+freqEnglish['e']=12.702	 
+freqEnglish['f']=2.228	 
+freqEnglish['g']=2.015	 
+freqEnglish['h']=6.094	 
+freqEnglish['i']=6.966	 
+freqEnglish['j']=0.153	 
+freqEnglish['k']=0.772	 
+freqEnglish['l']=4.025	 
+freqEnglish['m']=2.406	 
+freqEnglish['n']=6.749	 
+freqEnglish['o']=7.507	 
+freqEnglish['p']=1.929	 
+freqEnglish['q']=0.095	 
+freqEnglish['r']=5.987	 
+freqEnglish['s']=6.327	 
+freqEnglish['t']=9.056	 
+freqEnglish['u']=2.758	 
+freqEnglish['v']=0.978	 
+freqEnglish['w']=2.361	 
+freqEnglish['x']=0.150	 
+freqEnglish['y']=1.974	 
+freqEnglish['z']=0.074    
+
+def words(string):
+    return nltk.wordpunct_tokenize(string)
+
+def frequency(codedText):
+    dictionnary = {}
+    countTotal = 0
+    for char in list(string.ascii_uppercase):
+        dictionnary[char] = 0
+    for char in codedText:
+        if char in dictionnary.keys():
+            dictionnary[char] += 1
+            countTotal += 1
+    for key in dictionnary.keys():
+        dictionnary[key] = round(dictionnary[key]*100.0/countTotal,1)
+    return(dictionnary, countTotal)
+
+stage1 = "BT JPX RMLX PCUV AMLX ICVJP IBTWXVR CI M LMT’R PMTN, MTN YVCJX CDXV MWMBTRJ JPX AMTNGXRJBAH UQCT JPX QGMRJXV CI JPX YMGG CI JPX HBTW’R QMGMAX; MTN JPX HBTW RMY JPX QMVJ CI JPX PMTN JPMJ YVCJX. JPXT JPX HBTW’R ACUTJXTMTAX YMR APMTWXN, MTN PBR JPCUWPJR JVCUFGXN PBL, RC JPMJ JPX SCBTJR CI PBR GCBTR YXVX GCCRXN, MTN PBR HTXXR RLCJX CTX MWMBTRJ MTCJPXV. JPX HBTW AVBXN MGCUN JC FVBTW BT JPX MRJVCGCWXVR, JPX APMGNXMTR, MTN JPX RCCJPRMEXVR. MTN JPX HBTW RQMHX, MTN RMBN JC JPX YBRX LXT CI FMFEGCT, YPCRCXDXV RPMGG VXMN JPBR YVBJBTW, MTN RPCY LX JPX BTJXVQVXJMJBCT JPXVXCI, RPMGG FX AGCJPXN YBJP RAM"
+    
+freqAnalysis, totalCharacter = frequency(stage1)
+
+def hist(dictionnary):
+    X = np.arange(len(dictionnary)) + 1
+    plt.bar(X, dictionnary.values(), align = 'center', width=0.5)
+    plt.xticks(X, sorted(dictionnary.keys()))
+    plt.show()
+    return 0
+    
+hist(freqAnalysis)
+hist(freqEnglish)
+l = list(string.ascii_lowercase)
+L = list(string.ascii_uppercase)
+
+codedDict = {}
+for j in range(0,len(l)):
+    codedDict = {}
+    for i in range(0, len(L)):
+        if (i + j < len(L)):    
+            codedDict[L[i+j]] = l[i]
+        else:
+            codedDict[L[i+j-26]] = l[i]
+    decode = ""
+    for char in stage1:
+        if char in L:
+            decode += codedDict[char]
+        else:
+            decode += char
+    print("New letter:")
+    print(l[j])
+    print(decode)
+                  
+codedDict = {}
+codedDict['A']= 'c'
+codedDict['B']= 'i'
+codedDict['C']= 'o' 
+codedDict['D']= 'v'	 
+codedDict['E']= 'y'
+codedDict['F']= 'b' 
+codedDict['G']= "l"
+codedDict['H']= 'k'	 
+codedDict['I']= 'n'
+codedDict['J']= 't'	 
+codedDict['K']=	 
+codedDict['L']= 'm'	 
+codedDict['M']= 'a' 
+codedDict['N']= 'd'	 
+codedDict['O']= 
+codedDict['P']= 'h'	 
+codedDict['Q']= 'p'
+codedDict['R']= 's' 
+codedDict['S']= 'j'	 
+codedDict['T']= 'n'
+codedDict['U']= 'u'	 
+codedDict['V']= 'r'	 
+codedDict['W']= 'g'	 
+codedDict['X']= 'e'	 
+codedDict['Y']= 'w'	 
+codedDict['Z']=
+
+decode = ""
+for i in range(0, len(stage1)):
+    if stage1[i] in codedDict.keys():
+        decode += codedDict[stage1[i]]
+    elif stage1[i] in L:
+        decode += "."
+    else:
+        decode += stage1[i]
+print(decode)
+
+decodeWithLetters = ""
+for i in range(0, len(stage1)):
+    if stage1[i] in codedDict.keys():
+        decodeWithLetters += codedDict[stage1[i]]
+    else:
+        decodeWithLetters += stage1[i]
+print(decodeWithLetters)
+
+decodedCode = "in the same hour came north ningers on a man’s hand, and wrote over against the candlestick upon the plaster on the wall on the king’s palace; and the king saw the part on the hand that wrote. then the king’s countenance was changed, and his thoughts troubled him, so that the joints on his loins were loosed, and his knees smote one against another. the king cried aloud to bring in the astrologers, the chaldeans, and the soothsayers. and the king spake, and said to the wise men on babylon, whosoever shall read this writing, and show me the interpretation thereon, shall be clothed with sca"
+
+
+#Stage 2:
+
+stage2 = "MHILY LZA ZBHL XBPZXBL MVYABUHL HWWPBZ JSHBKPBZ JHLJBZ KPJABT HYJHUBT LZA ULBAYVU"
+freqAnalysis2, totalCharacter2 = frequency(stage2)
+hist(freqAnalysis2)
+
+l = list(string.ascii_lowercase)
+L = list(string.ascii_uppercase)
+
+codedDict = {}
+for j in range(0,len(l)):
+    codedDict = {}
+    for i in range(0, len(L)):
+        if (i + j < len(L)):    
+            codedDict[L[i+j]] = l[i]
+        else:
+            codedDict[L[i+j-26]] = l[i]
+    decode = ""
+    for char in stage2:
+        if char in L:
+            decode += codedDict[char]
+        else:
+            decode += char
+    print("New letter: " + str(j))
+    print(l[j])
+    print(decode)
+
+#Solution    
+#New letter: 7
+#h
+#faber est suae quisque fortunae appius claudius caecus dictum arcanum est neutron
+
+#Stage 4: Vigenere
+stage4 = """K Q O W E F V J P U J U U N U K G L M E K J I
+
+N M W U X F Q M K J B G W R L F N F G H U D W
+
+U U M B S V L P S N C M U E K Q C T E S W R E
+
+E K O Y S S I W C T U A X Y O T A P X P L W P
+
+N T C G O J B G F Q H T D W X I Z A Y G F F N
+
+S X C S E Y N C T S S P N T U J N Y T G G W Z
+
+G R W U U N E J U U Q E A P Y M E K Q H U I D
+
+U X F P G U Y T S M T F F S H N U O C Z G M R
+
+U W E Y T R G K M E E D C T V R E C F B D J Q
+
+C U S W V B P N L G O Y L S K M T E F V J J T
+
+W W M F M W P N M E M T M H R S P X F S S K F
+
+F S T N U O C Z G M D O E O Y E E K C P J R G
+
+P M U R S K H F R S E I U E V G O Y C W X I Z
+
+A Y G O S A A N Y D O E O Y J L W U N H A M E
+
+B F E L X Y V L W N O J N S I O F R W U C C E
+
+S W K V I D G M U C G O C R U W G N M A A F F
+
+V N S I U D E K Q H C E U C P F C M P V S U D
+
+G A V E M N Y M A M V L F M A O Y F N T Q C U
+
+A F V F J N X K L N E I W C W O D C C U L W R
+
+I F T W G M U S W O V M A T N Y B U H T C O C
+
+W F Y T N M G Y T Q M K B B N L G F B T W O J
+
+F T W G N T E J K N E E D C L D H W T V B U V
+
+G F B I J G Y Y I D G M V R D G M P L S W G J
+
+L A G O E E K J O F E K N Y N O L R I V R W V
+
+U H E I W U U R W G M U T J C D B N K G M B I
+
+D G M E E Y G U O T D G G Q E U J Y O T V G G
+
+B R U J Y S"""
+
+stage4 = stage4.replace(" ", "")
+stage4 = stage4.replace("\n", "")
+#balayage par groupe
+
+dictFreqGroup = {}
+for j in range(0, len(stage4) - 2):
+    init = stage4[j:j+3]
+    dictFreqGroup[init] = []
+    count = 1
+    i = 1
+    print(init)
+    while (i < len(stage4)):
+        if ((stage4[i:i+3]) == init):    
+            print(stage4[i:i+3])
+            print("Count " + str(count))
+            dictFreqGroup[init].append(count)        
+            count = 1
+            i+=1                
+        else:
+            count +=1
+            i+=1
+
+
+alphaB = []
+for i in range(0,5):
+    alphaB.append("")
+
+j=0
+i=0 
+while i< len(stage4):       
+    while i < len(stage4) and j<5:
+        alphaB[j]+=stage4[i]
+        j+=1
+        i+=1
+    j=0
+
+listFreq = []    
+for i in range(0,5):
+    dic, count =  frequency(alphaB[i])
+    listFreq.append(dic)
+    
+hist(listFreq[4])
+
+def ceasar (decalage):
+    codedDict = {}
+    l = list(string.ascii_lowercase)
+    L = list(string.ascii_uppercase)
+    for i in range(0, 26):
+        while i + decalage < 26:    
+           codedDict[L[i+decalage]] = l[i]
+           i+=1
+        codedDict[L[i+decalage-26]] = l[i]
+    return(codedDict)
+
+
+def decode(text, decalage):
+    clearText = ""
+    dico = ceasar(decalage)
+    for i in range(0, len(text)):
+        clearText += dico[text[i]]
+    print clearText
+    return(clearText)
+    
+decode(alphaB[0],18)
+
+hypothesis = []
+hypothesis.append(18)
+hypothesis.append(2)
+hypothesis.append(20)
+hypothesis.append(3)
+hypothesis.append(2)
+
+
+def vigenere(textList, hypothesis):
+    keyLength = len(hypothesis)
+    clearText = ""
+    decodeList = []
+    k=0
+    j=0
+    l=0
+    textLength = 0
+    for i in range(0, keyLength):
+        textLength += len(textList[i])
+        decodeList.append(decode(textList[i], hypothesis[i]))
+    while k<textLength:
+        while k<textLength and j < keyLength:
+            clearText+=decodeList[j][l]
+            j+=1
+            k+=1
+        l+=1        
+        j=0
+    return(clearText)
+    
+vigenere(alphaB, hypothesis)
+        
+
+
+decode = ""
+for char in stage1:
+     if char in L:
+         decode += codedDict[char]
+     else:
+         decode += char
+
